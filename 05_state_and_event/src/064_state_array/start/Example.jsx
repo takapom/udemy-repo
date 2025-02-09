cca76a6cacb6721218eeb7a3e91edec4bd94702e
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-02-09 17:20:14 +0900

    チャプター59まで

--- 05_state_and_event/src/064_state_array/start/Example.jsx
+++ 05_state_and_event/src/064_state_array/start/Example.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+
+  const shuffle = () => {
+  const newnums = [ ...nums ];
+  const value = newnums.pop()
+  newnums.unshift(value); 
+  setNums(newnums);
+  }
 
   return (
     <>
-      
+      {nums}
+      <button onClick={shuffle}>シャッフルボタン</button>
     </>
   );
 };
---
+++ vite-project/src/Todo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Todo = (todo) => {
+  return <>{todo} </> 
+}
+
+export default Todo;
