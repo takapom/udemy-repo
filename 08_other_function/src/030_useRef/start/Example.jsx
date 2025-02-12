82cfc1ee4ddfa11d4fac1ef5f154bfa355caa36b
takapom <23610119yt@stu.yamato-u.ac.jp>
2025-02-12 15:38:22 +0900

    useReducer make

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -1,25 +1,67 @@
 import { useState } from "react";
+import { useRef } from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
+  const inputRef = useRef()
 
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button>
     </div>
   );
 };
 
+const Case2 = () => {
+  const [playing, setplaying] = useState(false)
+  return(
+    <>
+    <video style={{maxWidth: "100%"}}>
+    <source src="./sample.mp4"></source>
+    </video>
+    <button onClick={() => {
+      setplaying(prev => !prev);
+    }}>
+      {playing ? "stop" : "play"}
+    </button>
+    </>
+
+  )
+};
+
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
 
 export default Example;
+
+
+
+// import { useRef } from "react";
+
+// function App(){
+//   //userefを適用する変数を定義
+//   const inputref = useRef(null);
+//   //集中する関数を定義
+//   const focusInput = () => {
+//     inputref.current.focus();
+//   };
+
+// return(
+//   <>
+//   <input ref={inputref} type="text" placeholder="初期値"/>
+//   <button onClick={focusInput}>フォーカスを当てる</button>
+//   </>
+//   );
+// }
+
+// export default App;
--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useReducer } from "react";
+
 const Example = () => {
-  
+  const [state, setstate] = useState(0);
+  const [rstate, dispatch] = useReducer(prev => {
+    if (action === "+"){
+      return ++prev;
+    }else if (action === "-"){
+      return --prev;
+    }
+  },0);
+  const countup = () => {
+   return setstate(prev => ++prev)
+  }
+  const rcountup = () => {
+    dispatch('+');
+  }
+
+  const rcountdown = () => {
+    dispatch('-')
+  }
+
   return (
     <>
+    <div>
+    <h3>{state}</h3>
+    <button onClick={countup}>+</button>
+    </div>
+
+    <div>
+    <h3>{rstate}</h3>
+    <button onClick={rcountup}>+</button>
+    <button onClick={rcountdown}>-</button>
+    </div>
     </>
   );
 };
